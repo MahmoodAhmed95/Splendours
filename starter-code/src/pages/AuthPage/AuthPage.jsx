fd5a643a71685f3f0499e2a1d1f6a01c0c716e7e
imonsterette <imonsterette@me.com>
2023-08-20 16:46:24 +0300

    authpage style

--- starter-code/src/pages/AuthPage/AuthPage.jsx
+++ starter-code/src/pages/AuthPage/AuthPage.jsx
@@ -5,21 +5,29 @@ import LoginForm from "../../components/LoginForm/LoginForm";
 export default function AuthPage({ user, setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
-    <main>
-      <h1>AuthPage</h1>
+    <main className="auth-page">
+        <div className="logo-container">
+    <img src="http://i.imgur.com/Lp5HN9F.png" alt="Logo" className="logo" />
+    <h1>MONSTERRA</h1>
+    <h2>Where gardeners bloom and grow</h2>
+  </div>
+  <div className="auth-content">
       {showSignUp ? (
         <SignUpForm setUser={setUser} user={user} />
       ) : (
         <LoginForm setUser={setUser} user={user} />
       )}
       {showSignUp ? (
-        <p>already have an account?</p>
+        <p>Already have an account?</p>
       ) : (
-        <p>doesnt have an account?</p>
+        <p>Don't have an account?</p>
       )}
       <a onClick={() => setShowSignUp(!showSignUp)}>
         {showSignUp ? "Log In" : "Sign Up"}
       </a>
+      </div>
     </main>
   );
 }
+
+
